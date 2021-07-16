628497e02fceab71edfd94a89d985da1f016e0cc
pawel.kania <pawel.kania@accenture.com>
2021-07-16 09:53:11 +0200

    project-structure

---
+++ cache/local/src/main/java/io/github/pavello/cache/local/LocalApplication.java
@@ -0,0 +1,13 @@
+package io.github.pavello.cache.local;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalApplication.class, args);
+	}
+
+}
